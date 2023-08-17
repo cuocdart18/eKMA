e8e3744f1cb4fbdd866b2872458ff223d77970bb
Cuoc Dat <ngoquocdat203@gmail.com>
2023-08-17 11:50:12 +0700

    add: chat paging demo

--- app/src/main/java/com/example/kmatool/ui/chat/main/ChatFragment.kt
+++ app/src/main/java/com/example/kmatool/ui/chat/main/ChatFragment.kt
@@ -1,13 +1,14 @@
 package com.example.kmatool.ui.chat.main
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kmatool.base.fragment.BaseFragment
+import com.example.kmatool.base.listeners.PaginationScrollListener
 import com.example.kmatool.common.KEY_PASS_CHAT_ROOM_ID
 import com.example.kmatool.databinding.FragmentChatBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,6 +33,7 @@ class ChatFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         getBundleData()
         initViews()
+        initMessaging()
     }
 
     private fun getBundleData() {
@@ -54,18 +56,64 @@ class ChatFragment : BaseFragment() {
             binding.edtMessageInput.text.clear()
         }
 
-        listenMessageDocChanges()
+        binding.rcvMessages.addOnScrollListener(object :
+            PaginationScrollListener(linearLayoutManager) {
+            override fun loadMore() {
+                viewModel.isLoading = true
+                (viewModel.currentPage)++
+                loadNextPage()
+            }
+
+            override fun isLoading() = viewModel.isLoading
+
+            override fun isLastPage() = viewModel.isLastPage
+        })
     }
 
-    private fun listenMessageDocChanges() {
-        @SuppressLint("NotifyDataSetChanged")
-        val firstAddEleCallback: () -> Unit = {
-            binding.rcvMessages.scrollToPosition(viewModel.messages.size - 1)
+    private fun initMessaging() {
+        viewModel.getTotalMessageCount {
+            viewModel.getOlderMessage { itemCount ->
+                chatAdapter.notifyItemRangeInserted(0, itemCount)
+                binding.rcvMessages.scrollToPosition(viewModel.messages.size - 1)
+                viewModel.isLoading = false
+                if (viewModel.currentPage < viewModel.totalPage) {
+                    chatAdapter.addHeaderLoading()
+                } else {
+                    viewModel.isLastPage = true
+                }
+            }
         }
-        val notFirstAddEleCallback: (itemCount: Int) -> Unit = { itemCount ->
-            chatAdapter.notifyItemRangeInserted(viewModel.messages.size - itemCount, itemCount)
+
+        viewModel.observeMessageDocChanges(addEleCallback)
+    }
+
+    private fun loadNextPage() {
+        logError("page=${viewModel.currentPage}")
+        Handler().postDelayed({
+            chatAdapter.removeHeaderLoading()
+            viewModel.getOlderMessage { itemCount ->
+                if (viewModel.currentPage < viewModel.totalPage) {
+                    // itemCount + 1 (first item for loading)
+                    chatAdapter.notifyItemRangeInserted(0, itemCount + 1)
+                } else {
+                    chatAdapter.notifyItemRangeInserted(0, itemCount)
+                }
+                viewModel.isLoading = false
+                if (viewModel.currentPage < viewModel.totalPage) {
+                    chatAdapter.addHeaderLoading()
+                } else {
+                    viewModel.isLastPage = true
+                }
+            }
+        }, 600L)
+    }
+
+    private val addEleCallback: (itemCount: Int) -> Unit = { itemCount ->
+        chatAdapter.notifyItemRangeInserted(viewModel.messages.size - itemCount, itemCount)
+        if (itemCount == 0) {
+            binding.rcvMessages.scrollToPosition(viewModel.messages.size - 1)
+        } else {
             binding.rcvMessages.smoothScrollToPosition(viewModel.messages.size - 1)
         }
-        viewModel.listenMessageDocChanges(firstAddEleCallback, notFirstAddEleCallback)
     }
 }
--- app/src/main/java/com/example/kmatool/ui/chat/main/ChatViewModel.kt
+++ app/src/main/java/com/example/kmatool/ui/chat/main/ChatViewModel.kt
@@ -10,8 +10,10 @@ import com.example.kmatool.common.KEY_ROOMS_COLL
 import com.example.kmatool.common.KEY_ROOM_MESSAGE_COLL
 import com.example.kmatool.data.models.Message
 import com.example.kmatool.data.models.service.IProfileService
-import com.google.firebase.Timestamp
+import com.google.firebase.firestore.AggregateSource
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
@@ -22,15 +24,25 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.Date
 import javax.inject.Inject
+import kotlin.math.ceil
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     private val profileService: IProfileService
 ) : BaseViewModel() {
     override val TAG = ChatViewModel::class.java.simpleName
+    private lateinit var msgCollRef: CollectionReference
+    private lateinit var msgChangeRegis: ListenerRegistration
+    private lateinit var lastVisibleDoc: DocumentSnapshot
+
     var roomId = ""
+    var isLoading = false
+    var isLastPage = false
+
     val messages = mutableListOf<Message>()
-    private lateinit var msgChangeRegistration: ListenerRegistration
+    private val messagePerPage = 30L
+    var totalPage = 0
+    var currentPage = 1
 
     fun sendMessage(content: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -43,7 +55,6 @@ class ChatViewModel @Inject constructor(
             val docRoomRef = Data.firestore
                 .collection(KEY_ROOMS_COLL)
                 .document(roomId)
-
             docRoomRef
                 .collection(KEY_ROOM_MESSAGE_COLL)
                 .add(messageMap)
@@ -53,54 +64,110 @@ class ChatViewModel @Inject constructor(
         }
     }
 
-    fun listenMessageDocChanges(
-        firstAddEleCallback: () -> Unit,
-        notFirstAddEleCallback: (itemCount: Int) -> Unit
+    fun getTotalMessageCount(
+        callback: () -> Unit
     ) {
-        msgChangeRegistration = Data.firestore
+        // init collection reference
+        msgCollRef = Data.firestore
             .collection(KEY_ROOMS_COLL)
             .document(roomId)
             .collection(KEY_ROOM_MESSAGE_COLL)
+
+        msgCollRef.count()
+            .get(AggregateSource.SERVER)
+            .addOnSuccessListener { task ->
+                val totalMessage = task.count
+                totalPage = ceil(totalMessage.toDouble() / messagePerPage).toInt()
+                logInfo("totalPage=$totalPage")
+                callback()
+            }
+    }
+
+    fun observeMessageDocChanges(addEleCallback: (itemCount: Int) -> Unit) {
+        var isFirstGetMessage = true
+        msgChangeRegis = msgCollRef
             .orderBy(KEY_MESSAGE_TIMESTAMP_DOC, Query.Direction.ASCENDING)
             .addSnapshotListener { value, _ ->
+                if (isFirstGetMessage) {
+                    isFirstGetMessage = false
+                    return@addSnapshotListener
+                }
                 viewModelScope.launch(Dispatchers.IO) {
-                    if (value != null) {
-                        listenMessagesAdded(value, firstAddEleCallback, notFirstAddEleCallback)
-                    }
+                    if (value == null) return@launch
+                    listenMessagesAdded(value, addEleCallback)
                 }
             }
     }
 
     private suspend fun listenMessagesAdded(
         value: QuerySnapshot,
-        firstAddEleCallback: () -> Unit,
-        notFirstAddEleCallback: (itemCount: Int) -> Unit
+        addEleCallback: (itemCount: Int) -> Unit
     ) {
-        val count = messages.size
-        value.documentChanges.forEach {
-            if (it.type == DocumentChange.Type.ADDED) {
-                val serverTimestamp = it.document.getTimestamp(KEY_MESSAGE_TIMESTAMP_DOC)
-                val timestamp = serverTimestamp?.toDate() ?: Date()
-                val content = it.document.get(KEY_MESSAGE_CONTENT_DOC).toString()
-                val from = it.document.get(KEY_MESSAGE_FROM_DOC).toString()
-                val message = Message(timestamp, content, from)
-                logInfo(message.toString())
-                messages.add(message)
+        var itemCount = 0
+        value.documentChanges
+            .forEach {
+                if (it.type == DocumentChange.Type.ADDED) {
+                    val serverTimestamp = it.document.getTimestamp(KEY_MESSAGE_TIMESTAMP_DOC)
+                    val timestamp = serverTimestamp?.toDate() ?: Date()
+                    val content = it.document.get(KEY_MESSAGE_CONTENT_DOC).toString()
+                    val from = it.document.get(KEY_MESSAGE_FROM_DOC).toString()
+                    val message = Message(timestamp, content, from)
+                    logInfo("add $message")
+                    messages.add(message)
+                    itemCount++
+                }
             }
-        }
+        logInfo("listenMessagesAdded")
         withContext(Dispatchers.Main) {
-            if (count == 0) {
-                firstAddEleCallback()
-            } else {
-                notFirstAddEleCallback(messages.size - count)
-            }
+            addEleCallback(itemCount)
         }
+    }
+
+    fun getOlderMessage(addEleCallback: (itemCount: Int) -> Unit) {
+        val messagesTemp = mutableListOf<Message>()
+        if (this::lastVisibleDoc.isInitialized) {
+            msgCollRef
+                .orderBy(KEY_MESSAGE_TIMESTAMP_DOC, Query.Direction.DESCENDING)
+                .startAfter(lastVisibleDoc)
+                .limit(messagePerPage)
+                .get()
+                .addOnSuccessListener { query ->
+                    getMsgPerPage(query, messagesTemp)
+                    addEleCallback(messagesTemp.size)
+                }
+        } else {
+            msgCollRef
+                .orderBy(KEY_MESSAGE_TIMESTAMP_DOC, Query.Direction.DESCENDING)
+                .limit(messagePerPage)
+                .get()
+                .addOnSuccessListener { query ->
+                    getMsgPerPage(query, messagesTemp)
+                    addEleCallback(messagesTemp.size)
+                }
+        }
+    }
+
+    private fun getMsgPerPage(query: QuerySnapshot, messagesTemp: MutableList<Message>) {
+        query.documents.forEach { doc ->
+            val serverTimestamp = doc.getTimestamp(KEY_MESSAGE_TIMESTAMP_DOC)
+            val timestamp = serverTimestamp?.toDate() ?: Date()
+            val content = doc.get(KEY_MESSAGE_CONTENT_DOC).toString()
+            val from = doc.get(KEY_MESSAGE_FROM_DOC).toString()
+            val message = Message(timestamp, content, from)
+            logInfo("$message")
+            messagesTemp.add(message)
+        }
+        if (query.documents.isNotEmpty()) {
+            lastVisibleDoc = query.documents.last()
+        }
+        messagesTemp.reverse()
+        messages.addAll(0, messagesTemp)
     }
 
     override fun onCleared() {
         super.onCleared()
-        if (this::msgChangeRegistration.isInitialized) {
-            msgChangeRegistration.remove()
+        if (this::msgChangeRegis.isInitialized) {
+            msgChangeRegis.remove()
         }
     }
 }
